1bb89b11a215e4fdcd88ee8640945ee592f2d298
Amir Azizov <creator005@mail.ru>
2024-07-30 19:28:35 +0300

    fix hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import CardPage from "./components/CardPage.tsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
