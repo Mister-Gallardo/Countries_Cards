fc34ac6c7a7c2a9d4de4f70674933cd13f45f3f1
Amir Azizov <creator005@mail.ru>
2024-07-30 08:54:30 +0300

    beta version

---
+++ src/components/CardElem.tsx
@@ -0,0 +1,31 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { ICountry } from "../services/interfaces";
+
+const CardElem = ({ country }: { country: ICountry }) => (
+  <Card sx={{ minWidth: '300px', width: '40%' }}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image={country.flags.svg}
+        alt={country.flags.alt}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" fontWeight={600} component="div">
+          {`${country.name.common} (${country.flag}) `}
+        </Typography>
+        <Typography color="text.secondary" fontSize="18px">
+          {`Столица: ${country.capital[0]}`}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+);
+
+export default CardElem;
---
+++ src/services/interfaces.ts
@@ -0,0 +1,6 @@
+export interface ICountry {
+  name: { common: string };
+  capital: string[];
+  flag: string;
+  flags: { alt: string, svg: string };
+}
